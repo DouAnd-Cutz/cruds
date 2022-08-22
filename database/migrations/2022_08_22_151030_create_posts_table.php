065a2713ed4827ab0a44851c05c762439b466d96
DouAnd-Cutz <110060761+DouAnd-Cutz@users.noreply.github.com>
2022-08-22 09:20:17 -0600

    Montando Proyecto

--- database/migrations/2022_08_22_151030_create_posts_table.php
+++ database/migrations/2022_08_22_151030_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
             $table->string("Titulo",255);
             $table->string("slug",255);
             $table->text("content");
             $table->string("image");
             $table->enum("posted",['yes','not']);
+            $table->timestamps();
         });
     }
 
@@ -29,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('posts');
     }
 }
